4b62db337ff3f69be066ebc11224a56dc2c49611
kratossore069 <victorgabrielarostegui@gmail.com>
2019-10-16 12:47:30 +0100

    Objeto_Coche

---
+++ poo/src/poo/coche.java
@@ -0,0 +1,24 @@
+package poo;
+
+/**
+ *
+ * @author Víctor
+ */
+public class coche {
+    
+    int ruedas;
+    int largo;
+    int ancho;
+    int motor;
+    int peso;
+    
+    public coche(){
+        
+        ruedas = 4;
+        peso = 500;
+        motor = 1600;
+        largo = 5;
+        ancho = 3;
+    }
+    
+}
