f257ef2a46c14aa030b94719c0873409a322c02a
kratossore069 <victorgabrielarostegui@gmail.com>
2019-11-11 11:44:35 +0000

    Terminado el 17

--- Prct17.java
+++ Prct17.java
@@ -17,22 +17,37 @@ public class Prct17 {
     :  Crear un programa que reproduzca el algoritmo anterior*/
 
     public static void main(String[] args) {
-
-        int orden[] = {5, 2, 12, 14, 8, 9, 11};
+        
+        int orden[] = new int[7];
+        orden[0] = 5;
+        orden[1] = 2;
+        orden[2] = 12;
+        orden[3] = 14;
+        orden[4] = 8;
+        orden[5] = 9;
+        orden[6] = 11;
+        /*int orden[] = {5, 2, 12, 14, 8, 9, 11};
         Arrays.sort(orden);
 
         for (int numero : orden) {
             System.out.println(numero); //De esta forma ordenamos los números de un array
+        }*/
+        
+        System.out.println("DE LA FORMA MATEMÁTICA A LA HORA DE ORDENARLOS: ");
+        int aux=0;
+        for(int j=0;j<7;j++){ //Usando un método parecido a cambiar las variables
+            for(int i =0;i<7-1;i++){
+                if(orden[i]>orden[i+1]){
+                    aux=orden[i];
+                    orden[i]=orden[i+1];
+                    orden[i+1]=aux;
+                }
+                
+            }
         }
         
-        System.out.println("De la forma matemática: ");
-        String espacio = "";
-        int orden2[] = {5, 2, 12, 14, 8, 9, 11};
-        for(int i=0;i<orden2.length;i++){
-            if(orden2[0]>orden2[1]){
-                espacio = "" +orden2;
-                System.out.println(orden2);  //NO TERMINADO
-            }
+        for(int i=0;i<orden.length;i++){ //Salen ordenados los números
+            System.out.println(orden[i]);
         }
     }
 }
