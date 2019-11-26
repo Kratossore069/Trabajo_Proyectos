36394b00ac5a5612d016b496754ead07a5505c77
kratossore069 <victorgabrielarostegui@gmail.com>
2019-11-26 21:10:17 +0000

    Ejercicios de nuevo tema

---
+++ Tema4/src/tema4/MiMain.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema4;
+
+/**
+ *
+ * @author Víctor_Gabriel_Carvajal_Aróstegui
+ */
+
+class Perro{
+    double peso;
+    String nombre, raza;
+    private String pulmones;
+    private int edad;
+    
+    void operarPulmones(String nuevosPulmones){
+        pulmones = nuevosPulmones;
+    }
+    
+    void cumpleanios(){
+        edad++;
+    }
+    
+    void mostrar(){
+        System.out.println("Nombre: "+nombre+" Raza: "+raza+" Peso: "+peso+" Edad: "+edad);
+        System.out.println("Pulmones: "+pulmones);
+    }
+}
+public class MiMain {
+    public static void main(String[] args) {
+        
+        Perro perro = new Perro();
+        
+        perro.nombre = "Fufi";
+        perro.peso = 30;
+        perro.raza = "Labrador";
+        perro.operarPulmones("Nuevos pulmones");
+        
+        perro.mostrar();
+        perro.cumpleanios();
+        
+        Persona p = new Persona();
+        //Persona a = new Persona();
+        
+        p.apellido = "Martín";
+        p.nombre = "Juan";
+        p.edad = 21;
+        
+        System.out.println(p.apellido+" "+p.nombre+" "+p.edad);
+    }
+}
