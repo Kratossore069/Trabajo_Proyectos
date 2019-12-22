97dbf1f7de086cbd8825fc0a727af91dd96f7c89
kratossore069 <victorgabrielarostegui@gmail.com>
2019-12-22 13:59:58 +0000

    Primera parte

---
+++ IntentoJuego/src/intentojuego/MainIntento.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intentojuego;
+
+/**
+ *
+ * @author Víctor Gabriel Carvajal Aróstegui
+ */
+public class MainIntento {
+    public static void main(String[] args) {
+        
+        IntentoJuego juego = new IntentoJuego();
+        juego.iniciar();
+        
+    }
+}
