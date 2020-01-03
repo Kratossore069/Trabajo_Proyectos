2b81a1864b92d575a5e3470f45b2f2196c485fc1
kratossore069 <victorgabrielarostegui@gmail.com>
2020-01-03 13:18:48 +0000

    Botones

--- Ventana.java
+++ Ventana.java
@@ -8,6 +8,7 @@ package repaso;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,6 +18,11 @@ import javax.swing.SwingConstants;
  *
  * @author Víctor Gabriel Carvajal Aróstegui
  */
+
+/*Práctica pensada para la utilización posterior
+* de una ventana o una interfaz gráfica para Java
+*/
+
 public class Ventana extends JFrame {
 
     // Constructores
@@ -30,11 +36,13 @@ public class Ventana extends JFrame {
 
     //Métodos
     private void componentes() {
+        //Poner un panel para poner encima el texto
         JPanel panel = new JPanel();
         getContentPane().add(panel); //Ponemos un panel
         panel.setBackground(Color.black); //Color
         panel.setLayout(null); //Permite poner los textos donde se quiera
         
+        //Crear etiquetas de texto
         JLabel etiqueta = new JLabel("Buenas tardes", SwingConstants.CENTER); //Manda un mensaje y centra el texto
         etiqueta.setForeground(Color.yellow);
         etiqueta.setBounds(5,5,150,50); //Dimensiones de la pantalla y las letras
@@ -43,14 +51,25 @@ public class Ventana extends JFrame {
         etiqueta.setFont(new Font("arial",Font.ITALIC,15)); //Establecemos la fuente de text
         panel.add(etiqueta); //Agregamos la etiqueta
         
+        //Insertar una imagen en la etiqueta
         JLabel imagen = new JLabel(new ImageIcon("/home/victor/Escritorio/duck.jpg")); //Creamos un elemento con la imagen
         imagen.setBounds(10, 80, 200, 200); //Redimensionamos la imagen
         panel.add(imagen); //Insertamos la imagen
     }
+    
+    private void botones(){
+        //Crear botones para la interfaz
+        JButton boton = new JButton("Pulsar");
+        boton.setBounds(100, 100, 100, 40);//Dimensiones
+        boton.setEnabled(true);//Botón activado
+        boton.setMnemonic('a');//Alt + parámetro
+        //panel.add(boton); FALTA INSERTAR EL BOTÓN
+    }
 
     //Main
     public static void main(String[] args) {
-
+        
+        //Creamos la ventana
         Ventana v1 = new Ventana();
 
         v1.setVisible(true); //La ventana se hace visible
