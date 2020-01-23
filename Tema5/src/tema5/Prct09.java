8b0bd4a1f6231f2ada9ec44da17f2cd837728424
kratossore069 <victorgabrielarostegui@gmail.com>
2020-01-23 19:19:50 +0000

    Más ejercicios hechos

--- Tema5/src/tema5/Prct09.java
+++ Tema5/src/tema5/Prct09.java
@@ -6,8 +6,6 @@
 package tema5;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  *
@@ -17,7 +15,7 @@ public class Prct09 {
     public static void main(String[] args) {
         
         /*Práctica 9:  Hacer un programa que el usuario introduzca un texto por 
-        teclado y mediante expresiones regulares se determine si es un número válido.
+        teclado y medianteexpresiones regulares se determine si es un número válido.
         Observa que esto significa quedebe empezar por una cifra o por los símbolos +-    
         Que después aparezcan cifras y sólocifras salvo la coma: “,”  la cuál  nos serviría para delimitar los decimales. 
         Hacer dos versiones, en la primera debe haber una coincidencia completa en el String que elusuario nos pase, 
@@ -25,35 +23,7 @@ public class Prct09 {
         
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Introduzca el numero: ");
-        
-        String numerosolo = sc.nextLine();
-        
-        Pattern numero = Pattern.compile("(^[-+]?[0-9]+,?[0-9]+)");
-        
-        Matcher num = numero.matcher(numerosolo);
-        
-        if(num.find()){
-            System.out.println("Sale "+numerosolo);
-        }else{
-            System.out.println("Error");
-        }
-        
-        //////////////////////////////////
-        
-        System.out.println("Introduzca un número con letras: ");
-        
-        String numerocompania = sc.nextLine();
-        
-        Pattern numerillo = Pattern.compile("(^[0-9]+[A-Za-z]*$)");
-        
-        Matcher numer = numerillo.matcher(numerocompania);
-        
-        if(numer.find()){
-            System.out.println("Sale "+numerocompania);
-        }else{
-            System.out.println("Error");
-        }
+        //^[-+]?[0-9]+,?
         
     }
 }
