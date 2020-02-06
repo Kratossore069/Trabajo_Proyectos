9d2d5ace4578ffdd9757ad9add681e767cadbbeb
kratossore069 <victorgabrielarostegui@gmail.com>
2020-02-06 22:40:05 +0000

    Nueva tarea de clase

---
+++ Otros_Java/CuentaBancaria/src/cuentabancaria/CuentaBancaria.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuentabancaria;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Víctor Gabriel Carvajal Aróstegui
+ */
+public class CuentaBancaria {
+
+    private String ncuenta;
+    private String propietario;
+    private double salario;
+    private ArrayList<Movimiento> movimiento;
+    private HashMap<Date, Movimiento> mov;
+    
+}
---
+++ Otros_Java/CuentaBancaria/src/cuentabancaria/Movimiento.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuentabancaria;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Víctor Gabriel Carvajal Aróstegui
+ */
+public class Movimiento {
+    
+    private double cuantia;
+    private Date fecha;
+    String asunto;
+    
+}
