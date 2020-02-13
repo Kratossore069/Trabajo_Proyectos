43f99701c85bfd9ada3990041b7aca707426d882
kratossore069 <victorgabrielarostegui@gmail.com>
2020-02-13 12:29:29 +0000

    Más ejers

---
+++ Tema5/src/tema5/Prct22.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema5;
+
+/**
+ *
+ * @author Víctor Gabriel Carvajal Aróstegui
+ */
+
+class Partido{
+    
+    private int votos;
+    private String nombre;
+    
+    public Partido(){
+        
+    }
+    
+    
+}
+
+
+public class Prct22 {
+    /*
+    Práctica 22: El usuario debe ir introduciendo el nombre de cada partido político y el
+número de votos que ha tenido. Después se le mostrará un menú donde puede elegir
+introducir el nombre de un partido y que le muestre los votos que ha tenido. Así como
+introducir un límite inferior de votos y uno superior para que se le muestren los partidos y
+los votos que han tenido que estén dentro de los límites. Utilizar la estructura dinámica más
+apropiada para este caso. Justificar su elección entre comentarios
+    */
+}
