755f0f92df8f366c1c00050d94d054961750fc84
kratossore069 <victorgabrielarostegui@gmail.com>
2020-02-23 12:05:59 +0000

    Integrales

---
+++ Otros_Java/Integrales/src/integrales/Funcion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package integrales;
+
+/**
+ *
+ * @author Víctor Gabriel Carvajal Aróstegui
+ */
+public interface Funcion {
+    
+    public double f(double x);
+    
+}
