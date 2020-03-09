a73c5e1f7adcbd69e454945ee18657ca856b6709
kratossore069 <victorgabrielarostegui@gmail.com>
2020-03-09 21:30:55 +0000

    Ejercicio incompleto de mañana

---
+++ Tema6/src/tema6/Notas.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema6;
+
+/**
+ *
+ * @author Víctor Gabriel Carvajal Aróstegui
+ */
+@FunctionalInterface
+public interface Notas {
+    double obtener(int array[],int num1, int num2, int num3);
+}
--- Tema6/src/tema6/PruebaDeClase.java
+++ Tema6/src/tema6/PruebaDeClase.java
@@ -5,19 +5,31 @@
  */
 package tema6;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Víctor Gabriel Carvajal Aróstegui
  */
-public class PruebaDeClase {
+public class PruebaDeClase{
     public static void main(String[] args) {
+        
+        ArrayList<Integer> valores = new ArrayList<>();
+        
         double array[] = {0,2.5,5,7.5,10};
         
-        obtener(array,1,0,0);
+        obtener(array,1,0,0); //Método estático
         
         valores.stream().
                 distinct().
                 sorted().
                 forEach(p->System.out.printf("%.2f\n",p));
     }
+    
+    static void obtener(double a[], int num1, int num2, int num3){
+        
+    }
+    
+
+    
 }
