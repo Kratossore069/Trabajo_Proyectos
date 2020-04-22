9d5e743b5a912b9c8c573615f177c5e421013287
kratossore069 <victorgabrielarostegui@gmail.com>
2020-04-22 18:22:40 +0100

    Mejoras en calculadora y nuevo proyecto

--- Tema7/ControlesRuntime/src/controlesruntime/ControlesRuntime.java
+++ Tema7/ControlesRuntime/src/controlesruntime/ControlesRuntime.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package calculadorafxml;
+package controlesruntime;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -13,16 +13,16 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author Víctor Gabriel Carvajal Aróstegui
+ * @author carlos
  */
-public class CalculadoraFXML extends Application {
+public class ControlesRuntime extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        stage.setResizable(false); //No se puede redimensionar
+        
         stage.setScene(scene);
         stage.show();
     }
