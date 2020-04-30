0f942406336b0f5f189f294fb98430d6d6fec98a
kratossore069 <victorgabrielarostegui@gmail.com>
2020-04-30 19:51:02 +0100

    Avances en el juego y cambios

--- Tema7/ProyectoFlota/src/proyectoflota/FXMLDocumentController.java
+++ Tema7/ProyectoFlota/src/proyectoflota/FXMLDocumentController.java
@@ -7,6 +7,7 @@ package proyectoflota;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
@@ -28,5 +29,12 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void disparoJugador(ActionEvent event) {
+        
+        
+        
+    }
     
 }
---
+++ Tema7/ProyectoFlota/src/proyectoflota/programa/Gestor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoflota.programa;
+
+/**
+ *
+ * @author Víctor Gabriel Carvajal Aróstegui
+ */
+public class Gestor {
+    
+    public Tablero jugador;
+    public Tablero maquina;
+
+    public Gestor(Tablero jugador, Tablero maquina) {
+        this.jugador=new Tablero;
+        this.maquina=new Tablero;
+    }
+    
+    
+    
+}
