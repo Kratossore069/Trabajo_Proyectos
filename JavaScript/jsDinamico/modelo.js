f21d3e1f7c47c5ef6fbf3a8001e1ef108ed9db73
kratossore069 <victorgabrielarostegui@gmail.com>
2021-08-01 13:59:44 +0100

    Más trabajo y fetch con api de Pokémon

--- JavaScript/jsDinamico/modelo.js
+++ JavaScript/jsDinamico/modelo.js
@@ -1,3 +1,4 @@
+"use strict";
 class Elementos {
     /**
      * Metodo que crea el título de la página
@@ -6,6 +7,7 @@ class Elementos {
         // Creo el div
         var divTitulo = document.createElement('div');
         divTitulo.setAttribute('id', 'cabecera');
+        divTitulo.classList.add('cajaTitulo');
         document.body.appendChild(divTitulo);
 
         // Creo el título
@@ -21,11 +23,13 @@ class Elementos {
         // Creo el div
         var divCuerpo = document.createElement('div');
         divCuerpo.setAttribute('id', 'cuerpo');
+        divCuerpo.classList.add('cajaCuerpo');
         document.body.appendChild(divCuerpo);
 
         // Creo el cuerpo
         var cuerpo = document.createElement('h1');
-        cuerpo.textContent = "Este es el cuerpo de la página";
+        cuerpo.textContent = "Lorem Ipsum";
+
         document.getElementById('cuerpo').appendChild(cuerpo);
     }
 
@@ -36,15 +40,46 @@ class Elementos {
         // Creo el div
         var divFirma = document.createElement('div');
         divFirma.setAttribute('id', 'firma');
+        divFirma.classList.add('cajaPie');
         document.body.appendChild(divFirma);
 
         // Creo la firma
         var firma = document.createElement('h1');
-        firma.textContent = "Este es el pie de pagina";
+        firma.textContent = "Desarrollado por Kratossore";
+
+        var pie = document.createElement('p');
+        pie.textContent = "Copyright 2021";
+
         document.getElementById('firma').appendChild(firma);
+        document.getElementById('firma').appendChild(pie);
+    }
+
+    /**
+     * Funcion que muestra los Pokémon
+     */
+    static conApis() {
+
+        var numeroAleatorio = Math.floor(Math.random() * (500 - 1)) + 1;
+        const url = "https://pokeapi.co/api/v2/pokemon/";
+
+        fetch(url + numeroAleatorio)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                var nombreHtml = document.getElementById("cabecera");
+                nombreHtml.innerHTML = "<h2>Pokémon llamado " + data.name + "</h2>";
+
+                var fotoPokemon = document.getElementById("cuerpo");
+                fotoPokemon.innerHTML = "<img src=" + data.sprites.front_default + "></img>";
+
+
+            }
+            )
+            .catch(error => document.write("Existe un error => " + error));
     }
 }
 
 Elementos.creoTitulo();
 Elementos.creoCuerpo();
 Elementos.creoFirma();
+Elementos.conApis();
